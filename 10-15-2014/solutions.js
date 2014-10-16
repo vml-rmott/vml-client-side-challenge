6ae40eb194488ec9c1dcab478338ea7dfa14ed8f
Ryan Mott <rymo@298223.no-reply.drupal.org>
2014-10-16 07:30:20 -0500

    refactoring is ADDICTIVE, because tests!

--- 10-15-2014/solutions.js
+++ 10-15-2014/solutions.js
@@ -1,52 +1,42 @@
 // All JS code goes in this file.
 
 function palindrome(str){
-    var rstr = str.replace(/[^A-Za-z]/gi, '').split("").reverse().join("");
-    return str.replace(/[^A-Za-z]/gi, '').toUpperCase() == rstr.toUpperCase();
+    str = str.replace(/[^a-z]/gi, '').toUpperCase();
+    var rstr = str.split("").reverse().join("");
+    return str == rstr;
 }
 
 function coinDeterminer(num){
     var coins = 0;
-    while (num >= 25) {
-        num -= 25;
-        coins++;
+    var denom = [25, 10, 5, 1];
+    for (var coin, i = 0; coin = denom[i++]; i <= denom.length) {
+        var r = num % coin;
+        coins += (num - r) / coin;
+        num = r;
     }
-    while (num >= 10) {
-        num -= 10;
-        coins++;
-    }
-    while (num >= 5) {
-        num -= 5;
-        coins++;
-    }
-    return (coins + num);
+    return coins;
 }
 
 function countingMinutes(strArr){
-    var times = strArr.split("-");
-
-    var makeMinutes = function(strTime) {
+    var minutes = function(strTime) {
         var ss = strTime.split(":");
-        var h2 = parseInt(ss[0] * 60);
-        var bb = "" + ss[1].slice(2,4);
-        if (ss[0] == 12) {
-            h2 = 0;
-        }
-        if (bb == "pm" && ss[0] < 12) {
-            h2 += (12*60);
-        }
-        var y = parseInt(ss[1].slice(0,2));
-        return (h2 + y);
+        var hour = parseInt(ss[0]) % 12;
+        var min = parseInt(ss[1].slice(0,2));
+        var mer = ss[1].slice(2,4).toLowerCase();
+
+        if (mer == "pm") hour += 12;
+        return (hour * 60 + min);
     };
 
-    var startMinutes = makeMinutes(times[0]);
-    var endMinutes = makeMinutes(times[1]);
+    var times = strArr.split("-");
+    var start = minutes(times[0]);
+    var end = minutes(times[1]);
 
-    if (startMinutes >= endMinutes) {
-        endMinutes += 1440;
+    if (start >= end) {
+        end += 1440;
     }
 
-    return endMinutes - startMinutes;
+    return end - start;
 }
 
 module.exports.palindrome = palindrome;
